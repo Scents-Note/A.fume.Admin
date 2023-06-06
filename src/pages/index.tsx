a3f036034ba7a1f75ec747f3ede23ffb9fae1ed2
jongdae <asdfg9377@naver.com>
2023-06-06 12:07:39 +0900

    [Fix] Login Page 개발
    1. 아이디나 패스워드 입력하지 않았을 시 helper text 출력
    
    2. Material UI 컴포넌트 사용 (TextField, Button)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,94 +1,70 @@
+import { Login } from "@/api/Login";
+import { ERROR_LOGIN_1 } from "@/common/message/message";
+import { TextField, Button, FormHelperText } from "@mui/material";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const Login: React.FC = (props) => {
-  const [username, setUsername] = useState<string>();
-  const [password, setPassword] = useState<string>();
+export const HomePage: React.FC = (props) => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   const router = useRouter();
 
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
-    // event.preventDefault();
-    // // TODO: JWT 토큰을 받아오는 백엔드 로직 추가하기
-    router.push("/main");
+    e.preventDefault();
+
+    if (username.length === 0 || password.length === 0)
+      setErrorMessage(ERROR_LOGIN_1);
+    else {
+      setErrorMessage(null);
+      // TODO: JWT 토큰을 받아오는 백엔드 로직 추가하기
+      const response = await Login(username, password);
+      console.log(response);
+      // router.push("/main");
+    }
   };
 
   return (
     <form
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
+      className="w-full h-full flex flex-col gap-20 items-center justify-center"
       onSubmit={handleLogin}
     >
-      <div
-        style={{
-          display: "flex",
-          width: "700px",
-          height: "130px",
-          gap: "60px",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "20px",
-          }}
-        >
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-            }}
-          >
-            <label style={{ width: "120px", textAlign: "center" }}>
-              아이디
-            </label>
-            <input
-              style={{ width: "200px" }}
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-            }}
-          >
-            <label style={{ width: "120px", textAlign: "center" }}>
-              비밀번호
-            </label>
-            <input
-              style={{ width: "200px" }}
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+      <div className="flex w-700 h-200 gap-60 justify-center items-center">
+        <div className="flex flex-col justify-center items-center gap-40">
+          <TextField
+            className="w-200"
+            size="small"
+            label="아이디"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            className="w-200"
+            size="small"
+            label="비밀번호"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
-        <input
-          style={{ width: "130px", height: "130px" }}
+        <Button
+          className="w-120 h-120"
           type="submit"
-          value="로그인"
-        />
+          variant="contained"
+          color="primary"
+        >
+          로그인
+        </Button>
       </div>
+      <FormHelperText error={errorMessage !== null}>
+        {errorMessage}
+      </FormHelperText>
     </form>
   );
 };
 
-export default Login;
+export default HomePage;
