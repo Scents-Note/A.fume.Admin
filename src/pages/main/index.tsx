3aa0623c5d00bacb47533abbdd3f7ab7acce26cc
jongdae <asdfg9377@naver.com>
2023-06-06 20:56:30 +0900

    [Feat] Login이 되지 않았는데 MainPage에 접근할 경우 Login 페이지로 Redirection

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,3 +1,24 @@
-export const MainPage: React.FC = () => <h1>Welcome to Main Page!</h1>;
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+import { MainLayout } from '@/components/icons/pages/main/MainLayout';
+
+export const MainPage: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const loginToken = localStorage.getItem('token');
+
+    if (loginToken === null || loginToken === undefined) {
+      router.replace('/');
+    }
+  }, []);
+
+  return (
+    <MainLayout>
+      <div>test</div>
+    </MainLayout>
+  );
+};
 
 export default MainPage;
