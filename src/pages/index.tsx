b3db19aa208e99f57eea7cf2119341d247ddb631
jongdae <asdfg9377@naver.com>
2023-06-06 17:56:08 +0900

    [Feat] login 페이지 기능 개발
    1. text field 제거 버튼 추가
    2. 필드 입력 하지 않을 시 로그인 버튼 disabled
    3. 에러 메세지 출력
    4. 로그인 토큰 저장, 토큰 존재할 시 리다이렉션

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Login } from "@/api/Login";
-import { ERROR_LOGIN_1 } from "@/common/message/message";
-import { TextField, Button, FormHelperText } from "@mui/material";
-import axios from "axios";
+import { Login } from "@/common/api/Login";
+import { EXPIRED_TIME_FOR_LOGIN_TOKEN } from "@/common/constants/constants";
+import { Logo } from "@/components/icons/icons";
+import { Cancel } from "@mui/icons-material";
+import { TextField, Button, IconButton, InputAdornment } from "@mui/material";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useMemo, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,56 +16,105 @@ export const HomePage: React.FC = (props) => {
 
   const router = useRouter();
 
+  const isDisabledLogin = useMemo(
+    () => email.length === 0 && password.length === 0,
+    [email, password]
+  );
+
+  useEffect(() => {
+    const loginToken = localStorage.getItem("token");
+
+    if (loginToken !== null && loginToken !== undefined) {
+      const loginData = JSON.parse(loginToken);
+
+      if (loginData.expired < Date.now()) {
+        localStorage.removeItem("token");
+      } else {
+        router.replace("/main");
+      }
+    }
+  }, []);
+
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (email.length === 0 || password.length === 0)
-      setErrorMessage(ERROR_LOGIN_1);
-    else {
+    try {
       setErrorMessage(null);
       // TODO: JWT 토큰을 받아오는 백엔드 로직 추가하기
-      const response = axios.post("/api/admin/login", { email, password });
-      console.log(response);
-      // router.push("/main");
+      const response = await Login(email, password);
+
+      const loginData = JSON.stringify({
+        token: response.data.token,
+        expired: Date.now() + EXPIRED_TIME_FOR_LOGIN_TOKEN,
+      });
+      localStorage.setItem("token", loginData);
+
+      router.push("/main");
+    } catch (error: any) {
+      setErrorMessage(error.message);
     }
   };
 
   return (
-    <form
-      className="w-full h-full flex flex-col gap-20 items-center justify-center"
-      onSubmit={handleLogin}
-    >
-      <div className="flex w-700 h-200 gap-60 justify-center items-center">
-        <div className="flex flex-col justify-center items-center gap-40">
-          <TextField
-            className="w-200"
-            size="small"
-            label="아이디"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            className="w-200"
-            size="small"
-            label="비밀번호"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <Button
-          className="w-120 h-120"
-          type="submit"
-          variant="contained"
-          color="primary"
+    <div className="w-full h-full flex justify-center items-center bg-login-background">
+      <div className="w-600 h-500 flex justify-center items-center rounded-4 shadow-paper bg-white">
+        <form
+          className="w-full h-full flex flex-col items-center justify-center"
+          onSubmit={handleLogin}
         >
-          로그인
-        </Button>
+          <Logo className="mb-8" />
+          <div className="mb-50 select-none">관리자 페이지</div>
+          <div className="flex flex-col gap-20">
+            <TextField
+              className="w-500"
+              size="small"
+              label="어드민 계정"
+              value={email}
+              error={errorMessage !== null}
+              InputProps={{
+                endAdornment: email && (
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => setEmail("")}>
+                      <Cancel />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              className="w-500"
+              size="small"
+              label="비밀번호"
+              type="password"
+              value={password}
+              error={errorMessage !== null}
+              helperText={errorMessage}
+              InputProps={{
+                endAdornment: password && (
+                  <InputAdornment position="end">
+                    <IconButton onClick={() => setPassword("")}>
+                      <Cancel />
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              className="w-500"
+              disabled={isDisabledLogin}
+              type="submit"
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              로그인
+            </Button>
+          </div>
+        </form>
       </div>
-      <FormHelperText error={errorMessage !== null}>
-        {errorMessage}
-      </FormHelperText>
-    </form>
+    </div>
   );
 };
 
